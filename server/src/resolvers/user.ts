1ac3d75fa11cbf59ed4fae646a0ca147f080bdd6
Abderraouf Zine <rofazayn@gmail.com>
2021-09-07 14:59:16 +0100

    create auth resolvers

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,86 @@
+import argon2 from 'argon2';
+import { User } from '../entities/User';
+import { MyContext } from '../types';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from 'type-graphql';
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          { field: 'username', message: 'Incorrect login credentials.' },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Incorrect login credentials.',
+          },
+        ],
+      };
+    }
+
+    return { user };
+  }
+}
