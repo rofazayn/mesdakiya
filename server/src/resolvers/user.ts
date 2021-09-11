f317685e4b898ff84a27e4b799f537d1b1731d6c
Abderraouf Zine <rofazayn@gmail.com>
2021-09-11 17:23:44 +0100

    feat(server): handle server-side register validation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,7 @@
 import argon2 from 'argon2';
 import { User } from '../entities/User';
 import { MyContext } from '../types';
+import * as yup from 'yup';
 import {
   Arg,
   Ctx,
@@ -43,14 +44,44 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
+    const registerValidationSchema = yup.object().shape({
+      username: yup.string().required().min(4).max(20),
+      password: yup.string().required().min(6).max(40),
+    });
+
+    // validate inputs
+    try {
+      await registerValidationSchema.validate(options).then();
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return {
+          errors: [{ field: error.path as string, message: error.message }],
+        };
+      } else {
+        throw error;
+      }
+    }
+
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
+
+    // duplicate username check
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      if ((error.code = '23505')) {
+        return {
+          errors: [{ field: 'username', message: 'username already in use.' }],
+        };
+      }
+    }
+
+    req.session.userId = user.id;
 
     return { user };
   }
